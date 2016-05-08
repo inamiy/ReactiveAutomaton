42a244caac1f52f921c3b11930b55542f91b85b4
Yasuhiro Inami <inamiy@gmail.com>
2016-05-09 02:50:02 +0900

    Simplify `sampleFrom`

--- Sources/ReactiveCocoa+SampleFrom.swift
+++ Sources/ReactiveCocoa+SampleFrom.swift
@@ -9,62 +9,36 @@
 import Result
 import ReactiveCocoa
 
-private struct SampleState<Value> {
-    var latestValue: Value? = nil
-    var signalCompleted: Bool = false
-    var sampleeCompleted: Bool = false
-}
+// MARK: sampleFrom http://rxmarbles.com/#withLatestFrom
+
+// NOTE:
+// `sampleFrom` (Rx.withLatestFrom) works similar to `combineLatest` + `sampleOn` as discussed in
+// https://github.com/ReactiveCocoa/ReactiveCocoa/pull/2082#issuecomment-112301993 ,
+// but it should rather NOT be affected by `samplee`'s termination events.
+// That also means, `samplee` should be restricted to have `NoError` as error type.
 
 extension SignalType {
 
-    /// Flipped version of `sampleOn`, combining receiver's (sampler) and `samplee`'s values.
+    /// Flipped version of `sampleOn`, combining `self`'s (sampler) and `samplee`'s values.
+    /// - Returns: A signal that terminates only when `self` terminates (`samplee` doesn't affect).
     @warn_unused_result(message="Did you forget to call `observe` on the signal?")
     internal func sampleFrom<Value2>(samplee: Signal<Value2, NoError>) -> Signal<(Value, Value2), Error> {
         return Signal { observer in
-            let state = Atomic(SampleState<Value2>())
+            let state = Atomic<Value2?>(nil)
             let disposable = CompositeDisposable()
 
-            disposable += samplee.observe { event in
-                switch event {
-                case let .Next(value):
-                    state.modify { st in
-                        var mutableSt = st
-                        mutableSt.latestValue = value
-                        return mutableSt
-                    }
-                case .Completed:
-                    let oldState = state.modify { st in
-                        var mutableSt = st
-                        mutableSt.sampleeCompleted = true
-                        return mutableSt
-                    }
-
-                    if oldState.signalCompleted {
-                        observer.sendCompleted()
-                    }
-                case .Interrupted:
-                    observer.sendInterrupted()
-                default:
-                    break
-                }
+            disposable += samplee.observeNext { value in
+                state.value = value
             }
 
             disposable += self.observe { event in
                 switch event {
                 case let .Next(value):
-                    if let value2 = state.value.latestValue {
+                    if let value2 = state.value {
                         observer.sendNext((value, value2))
                     }
                 case .Completed:
-                    let oldState = state.modify { st in
-                        var mutableSt = st
-                        mutableSt.signalCompleted = true
-                        return mutableSt
-                    }
-
-                    if oldState.sampleeCompleted {
-                        observer.sendCompleted()
-                    }
+                    observer.sendCompleted()
                 case let .Failed(error):
                     observer.sendFailed(error)
                 case .Interrupted:
@@ -76,7 +50,8 @@ extension SignalType {
         }
     }
 
-    /// Flipped version of `sampleOn`, combining receiver's (sampler) and `samplee`'s values.
+    /// Flipped version of `sampleOn`, combining `self`'s (sampler) and `samplee`'s values.
+    /// - Returns: A signal that terminates only when `self` terminates (`samplee` doesn't affect).
     @warn_unused_result(message="Did you forget to call `observe` on the signal?")
     internal func sampleFrom<Value2>(samplee: SignalProducer<Value2, NoError>) -> Signal<(Value, Value2), Error> {
         return Signal { observer in
@@ -92,7 +67,15 @@ extension SignalType {
 
 extension SignalProducerType {
 
-    /// Flipped version of `sampleOn`, combining receiver's (sampler) and `samplee`'s values.
+    /// Flipped version of `sampleOn`, combining `self`'s (sampler) and `samplee`'s values.
+    /// - Returns: A signalProducer that terminates only when `self` terminates (`samplee` doesn't affect).
+    @warn_unused_result(message="Did you forget to call `start` on the producer?")
+    internal func sampleFrom<Value2>(samplee: Signal<Value2, NoError>) -> SignalProducer<(Value, Value2), Error> {
+        return lift(Signal.sampleFrom)(samplee)
+    }
+
+    /// Flipped version of `sampleOn`, combining `self`'s (sampler) and `samplee`'s values.
+    /// - Returns: A signalProducer that terminates only when `self` terminates (`samplee` doesn't affect).
     @warn_unused_result(message="Did you forget to call `start` on the producer?")
     internal func sampleFrom<Value2>(samplee: SignalProducer<Value2, NoError>) -> SignalProducer<(Value, Value2), Error> {
         return lift(Signal.sampleFrom)(samplee)
